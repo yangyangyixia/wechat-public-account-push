64296033302472b8055d7706160826eb4ca098eb
yangyangyixia <39325012+yangyangyixia@users.noreply.github.com>
2022-12-03 21:08:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,7 +16,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKKxU6HoMBE6SCn6BWREJ0uJVcAg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7AwaAoDhDnU1rO8zpEA1wOLV-W6_M6HV7_jmCdtiZGE',
+      useTemplateId: '1dKLbqXbUgzVYs7tQH9kIF2DZP33Ba5BZEjKuoPdSsI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-29',
      
